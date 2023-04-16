62375c8d16a8c1261f3649de5f0e7cbb4f399d8f
Георгий Сапко <arkalis@yandex.ru>
2023-04-16 16:05:58 +0300

    1st review

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from posts.models import Comment, Group, Post
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'author', 'post', 'text', 'created')
+        read_only_fields = ('author', 'post',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('title', 'slug', 'description', 'id')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True,
+    )
+
+    class Meta:
+        model = Post
+        fields = ('id', 'text', 'author', 'group', 'pub_date')
+        read_only_fields = ('author',)
